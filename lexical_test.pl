0e569974f12fc9725f7ef908b08fb21586aa80c7
Eduardo Moya <e.d.mobe@hotmail.com>
2019-05-28 15:58:47 -0600

    9

--- lexical_test.pl
+++ lexical_test.pl
@@ -1,5 +1,5 @@
 /*
- * Ejemplo 6.2.8.
+ * Ejemplo 6.2.9.
  * Se busca simplificar la notacion de las gramaticas libres de contexto
  * Segun lo investigado, hay otra manera de expresar las gramaticas
  * Se implementa logica que permite utilizar los caminos como nombres
@@ -12,58 +12,79 @@
  * generado varios problemas
  * Se incluye la logica de interpretacion de oraciones
  * Se soluciona el problema de palabras compuestas
+ * Se incluye la descomposicion de una oracion en gramaticas
  */
 
-concatenar([], L, L).
-concatenar([X|L1], L2, [X|L3]) :- concatenar(L1, L2, L3).
-
+% Base de datos
 camino --> [[cartago], [san, jose], 25];
            [[alajuela], [heredia], 7];
            [[san, jose], [alajuela], 15];
            [[cartago], [tres, rios], 10].
 
+% Todo primer elemento en los nodos es un origen
 origen(S,_) :- camino([S|_],_).
+% Todo segundo elemento en los nodos es un destino
 destino(S,_) :- camino([_,S|_],_).
-
+% Un origen o un destino son lugares
 lugar --> origen; destino.
 
-% El problema es que no se pueden concatenar listas como lo estoy haciendo
-% Analogo a: oracion(A, B) :- sintagma_nominal(A,C), sintagma_verbal(C,B).
-oracion(PalabrasClave, Frase, []) :- sintagma_nominal(_, Numero, PalabrasClave1, Frase1, []),
-                                      sintagma_verbal(Numero, PalabrasClave2, Frase2, []),
-                                      concatenar(PalabrasClave1, PalabrasClave2, PalabrasClave),
-                                      concatenar(Frase1, Frase2, Frase);
-                                      sintagma_nominal(_, _, PalabrasClave, Frase, []);
-                                      sintagma_verbal(_, PalabrasClave, Frase, []).
+% Metodos necesarios
+% Concatena dos listas
+concatenar([], L, L).
+concatenar([X|L1], L2, [X|L3]) :- concatenar(L1, L2, L3).
 
-/*
-oracion([PalabrasClave1, PalabrasClave2]) --> sintagma_nominal(_, Numero, PalabrasClave1), sintagma_verbal(Numero, PalabrasClave2).
-oracion([PalabrasClave]) --> sintagma_nominal(_, _, PalabrasClave).
-oracion([PalabrasClave]) --> sintagma_verbal(_, PalabrasClave).
-*/
+% Elimina el elemento L de una lista [H|T].
+remove_elements(L, [H|T], R) :-
+    delete(L, H, R1),
+    remove_elements(R1, T, R).
+remove_elements(L, [], L).
 
-% Caso 1: "el origen / es ..."
-% Caso 2: "mi origen / es ..."
+validar(Categoria, Frase) :- phrase(oracion(_, Gramatica), Frase),
+                             phrase(gramatica_valida(Categoria), Gramatica).
+
+oracion(PalabrasClaveFinal, GramaticaFinal, Frase, []) :-
+    phrase(oracion_auxiliar(PalabrasClave, Gramatica), Frase),
+    list_to_set(Gramatica, GramaticaOrdenada),
+    remove_elements(PalabrasClave, [[]], PalabrasClaveFinal),
+    remove_elements(GramaticaOrdenada, [[]], GramaticaFinal).
+
+% Se definen las tres posibles gramaticas para una oracion
+oracion_auxiliar(PalabrasClave, Gramatica, Frase, []) :- sintagma_nominal(_, Numero, PalabrasClave1, Gramatica1, Frase1, []),
+                                                sintagma_verbal(Numero, PalabrasClave2, Gramatica2, Frase2, []),
+                                                concatenar(PalabrasClave1, PalabrasClave2, PalabrasClave),
+                                                concatenar(Gramatica1, Gramatica2, Gramatica),
+                                                concatenar(Frase1, Frase2, Frase);
+                                                sintagma_nominal(_, _, PalabrasClave, Gramatica, Frase, []);
+                                                sintagma_verbal(_, PalabrasClave, Gramatica, Frase, []).
+
+% Sintagmas nominales y sus gramaticas
+% Caso 1: "el origen"
+% Caso 2: "mi origen"
 % Caso 3: "Cartago"
-% Caso 4: "yo / voy ..."
-sintagma_nominal(Genero, Numero, [PalabrasClave]) --> determinante(Genero, Numero), nombre(Genero, Numero, PalabrasClave);
-                                                      determinante(impersonal, Numero), nombre(_Genero2, Numero, PalabrasClave);
-                                                      nombre(Genero, Numero, PalabrasClave).
-sintagma_nominal(Genero, Numero, [[]]) --> determinante(Genero, Numero).
+% Caso 4: "yo"
+sintagma_nominal(Genero, Numero, [PalabrasClave], [Gramatica]) --> determinante(Genero, Numero), nombre(Genero, Numero, PalabrasClave, Gramatica);
+                                                                   determinante(impersonal, Numero), nombre(_Genero2, Numero, PalabrasClave, Gramatica);
+                                                                   nombre(Genero, Numero, PalabrasClave, Gramatica).
+sintagma_nominal(Genero, Numero, [[]], [[]]) --> determinante(Genero, Numero).
 
+% Sintagmas verbales y sus gramaticas
 % Caso 1: "no"
 % Caso 2: "para Cartago"
 % Caso 3: "es Cartago"
 % Caso 4: "no voy para Cartago"
-% Caso 5: "voy PARA Cartago"
-sintagma_verbal(_, [PalabrasClave]) --> modulizador(PalabrasClave).
-sintagma_verbal(_, [PalabrasClave1, PalabrasClave2]) --> enlace(PalabrasClave1), sintagma_nominal(_, _, [PalabrasClave2]).
-sintagma_verbal(Numero, [PalabrasClave1, PalabrasClave2]) -->
-    verbo_transitivo(Numero, PalabrasClave1), sintagma_nominal(_, Numero, [PalabrasClave2]).
-sintagma_verbal(Numero, [PalabrasClave1, PalabrasClave2, PalabrasClave3]) -->
-    verbo_transitivo(Numero, PalabrasClave1), enlace(PalabrasClave2), sintagma_nominal(_, _, [PalabrasClave3]).
-sintagma_verbal(Numero, [PalabrasClave1, PalabrasClave2, PalabrasClave3, PalabrasClave4]) -->
-    modulizador(PalabrasClave1), verbo_transitivo(Numero, PalabrasClave2), enlace(PalabrasClave3), sintagma_nominal(_, _, [PalabrasClave4]).
+% Caso 5: "voy para Cartago"
+sintagma_verbal(_, [PalabrasClave], [Gramatica]) --> modulizador(PalabrasClave, Gramatica).
+sintagma_verbal(_, [PalabrasClave1, PalabrasClave2], [Gramatica1, Gramatica2]) -->
+     enlace(PalabrasClave1, Gramatica1), sintagma_nominal(_, _, [PalabrasClave2], [Gramatica2]).
+sintagma_verbal(Numero, [PalabrasClave1, PalabrasClave2], [Gramatica1, Gramatica2]) -->
+    verbo_transitivo(Numero, PalabrasClave1, Gramatica1), sintagma_nominal(_, Numero, [PalabrasClave2], [Gramatica2]).
+sintagma_verbal(Numero, [PalabrasClave1, PalabrasClave2, PalabrasClave3], [Gramatica1, Gramatica2, Gramatica3]) -->
+    verbo_transitivo(Numero, PalabrasClave1, Gramatica1), enlace(PalabrasClave2, Gramatica2), sintagma_nominal(_, _, [PalabrasClave3], [Gramatica3]).
+sintagma_verbal(Numero, [PalabrasClave1, PalabrasClave2, PalabrasClave3, PalabrasClave4], [Gramatica1, Gramatica2, Gramatica3, Gramatica4]) -->
+    modulizador(PalabrasClave1, Gramatica1),
+    verbo_transitivo(Numero, PalabrasClave2, Gramatica2),
+    enlace(PalabrasClave3, Gramatica3),
+    sintagma_nominal(_, _, [PalabrasClave4], [Gramatica4]).
 
 % Determinantes
 determinante(impersonal, singular) --> [mi].
@@ -71,94 +92,55 @@ determinante(masculino, singular) --> [el]; [un]; [yo]; [me].
 determinante(femenino, singular) --> [una]; [la].
 
 % Verbos
-% verbo_transitivo(GeneroSinonimo) --> sinonimo_verbo_transitivo(GeneroSinonimo, _).
-% FALTAN SINONIMOS
-verbo_transitivo(singular, []) --> [es]; [se,ubica].
-verbo_transitivo(singular, [ubico]) --> [ubico].
-verbo_transitivo(singular, [voy]) --> [voy].
-verbo_transitivo(singular, [estoy]) --> [estoy].
-verbo_transitivo(singular, [ir]) --> [ir].
+verbo_transitivo(singular, [], []) --> [es]; [se,ubica].
+verbo_transitivo(singular, [ubico], [origen]) --> [ubico].
+verbo_transitivo(singular, [voy], [destino]) --> [voy].
+verbo_transitivo(singular, [encamino], [destino]) --> [encamino].
+verbo_transitivo(singular, [dirijo], [destino]) --> [dirijo].
+verbo_transitivo(singular, [estoy], [origen]) --> [estoy].
+verbo_transitivo(singular, [encuentro], [origen]) --> [encuentro].
+verbo_transitivo(singular, [ir], [destino]) --> [ir].
 
 % Nombres
-% Se manejaran casos con mayuscula desde Java.
-% FALTAN SINONIMOS
-nombre(masculino, singular, [origen]) --> [origen].
-nombre(masculino, singular, [destino]) --> [destino].
-nombre(masculino, singular, Lugar, Lugar, _) :- phrase(lugar, Lugar).
-nombre(femenino, singular, [parada]) --> [parada].
+nombre(masculino, singular, [origen], [origen]) --> [origen].
+nombre(masculino, singular, [punto, de, partida], [origen]) --> [punto, de, partida].
+nombre(masculino, singular, [destino], [destino]) --> [destino].
+nombre(masculino, singular, Lugar, [lugar], Lugar, _) :- phrase(lugar, Lugar).
+nombre(femenino, singular, [parada], [parada]) --> [parada].
+nombre(masculino, singular, [punto, de, partida], [origen]) --> [punto, de, partida].
 
 % Modulizadores
-% modulizador --> sinonimo_modulizador(_).
-% FALTAN SINONIMOS
-modulizador([si]) --> [si].
-modulizador([no]) --> [no].
+modulizador([si], [afirmacion]) --> [si].
+modulizador([afirmativo], [afirmacion]) --> [afirmativo].
+modulizador([claro], [afirmacion]) --> [claro].
+modulizador([de, acuerdo], [afirmacion]) --> [de, acuerdo].
+modulizador([por, supuesto], [afirmacion]) --> [por, supuesto].
+modulizador([no], [negacion]) --> [no].
+modulizador([negativo], [negacion]) --> [negativo].
 
 % Enlaces
-% enlace --> sinonimo_enlace(_).
-% FALTAN SINONIMOS
-enlace([para]) --> [para].
-enlace([en]) --> [en].
+enlace([para], [destino]) --> [para].
+enlace([hacia], [destino]) --> [hacia].
+enlace([a], [destino]) --> [a].
+enlace([en], [origen]) --> [en].
 
-/*
-% palabra --> lugar; determinante(_, _); verbo_transitivo(_); nombre(_,
-% _); modulizador; enlace.
+% Falta quitar elementos vacios de una lista
+% Falta quitar elementos repetidos de una lista
+% Falta validar gramaticas e interpretarlas
+% Falta hacer conversacion
 
-% Sinonimos para verbos transitivos
-sinonimo(verbo_transitivo, singular, ir) --> [quiero,ir].
-sinonimo(verbo_transitivo, singular, voy) --> [dirijo].
-sinonimo(verbo_transitivo, singular, estoy) --> [encuentro].
-
-% Sinonimos de nombres
-sinonimo(nombre, masculino, singular, origen) --> [punto,de,partida].
-sinonimo(nombre, masculino, singular, parada) --> [punto,intermedio]; [destino,intermedio].
-
-% Sinonimos de modulizadores
-sinonimo(modulizador, si) --> [afirmativo]; [claro]; [de,acuerdo]; [por,supuesto].
-sinonimo(modulizador, no) --> [negativo].
-
-% Sinonimos de enlaces
-sinonimo(enlace, para) --> [a].
-
-
-% Palabras clave, significado
-palabra_clave([origen], origen).
-palabra_clave([destino], destino).
-palabra_clave([parada], parada).
-palabra_clave([a], destino).
-palabra_clave([voy], destino).
-palabra_clave([estoy], origen).
-palabra_clave([ir], destino).
-palabra_clave([ubico], origen).
-palabra_clave([si], afirmacion).
-palabra_clave([no], negacion).
-palabra_clave([para], destino).
-palabra_clave(Lugar, lugar_origen) :- origen(Lugar, []).
-palabra_clave(Lugar, lugar_destino) :- destino(Lugar, []).
-palabra_clave(Lugar, lugar_parada) :- origen(Lugar, []), destino(Lugar, []).
-%faltan sinonimos!!!!!!!!
-
-gramatica_valida(origen) --> [origen], [lugar_origen];
-                             [lugar_origen], [origen].
-gramatica_valida(destino) --> [destino], [lugar_destino];
-                              [lugar_destino], [destino].
-gramatica_valida(parada) --> [parada], [lugar_parada];
-                             [lugar_parada], [parada].
+gramatica_valida(origen) --> [[origen], [lugar]];
+                             [[lugar], [origen]].
+gramatica_valida(destino) --> [[destino], [lugar]];
+                              [[lugar], [destino]].
+gramatica_valida(parada) --> [[parada], [lugar]];
+                             [[lugar], [parada]].
 gramatica_valida(confirmacion) --> [afirmacion];
                                    [negacion].
+
+
+
 /*
-extraer_palabras_clave(L, LE) :- extraer_palabras_clave(L, [], LE).
-extraer_palabras_clave([], LE, LE).
-extraer_palabras_clave([X,Y|L1], L2, L3) :- (palabra([X], _) -> (palabra_clave([X], _) -> extraer_palabras_clave([Y|L1], [X|L2], L3); extraer_palabras_clave([Y|L1], L2, L3)); extraer_palabras_clave([[X,Y]], L2, L3)).
-*/
-
-categorizar_palabras_clave(L, LC) :- categorizar_palabras_clave(L, [], LC).
-categorizar_palabras_clave([], LC, LC).
-categorizar_palabras_clave([X|L1], L2, L3) :- (palabra_clave([X], T) -> categorizar_palabras_clave(L1, [T|L2], L3); categorizar_palabras_clave(L1, L2, L3)).
-
-gramatica_palabras_clave(L, G, []) :- categorizar_palabras_clave(L, L1), sort(L1, G).
-
-autollenar(origen, Oracion, Resultado) :- concatenar([estoy, en], Oracion, Resultado).
-
 main :- writeln("Origen?"),
         validar(origen),
         write(" efectivamente es un origen.\n").
@@ -174,6 +156,7 @@ validar(Objetivo) :- repeat,
                                 true;
                                 write("Comprendo lo que dices pero no puedo responderte con esa informacion. "), preguntar(Objetivo), fail);
                            writeln("No te entiendo. "), preguntar(Objetivo), fail).
+
 */
 
 /*
