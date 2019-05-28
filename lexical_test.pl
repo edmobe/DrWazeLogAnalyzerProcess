820ad3a86e14ec354c46928c252cb4adad6e9c0c
Eduardo Moya <e.d.mobe@hotmail.com>
2019-05-28 15:58:17 -0600

    8

--- lexical_test.pl
+++ lexical_test.pl
@@ -1,5 +1,5 @@
 /*
- * Ejemplo 6.2.7.
+ * Ejemplo 6.2.8.
  * Se busca simplificar la notacion de las gramaticas libres de contexto
  * Segun lo investigado, hay otra manera de expresar las gramaticas
  * Se implementa logica que permite utilizar los caminos como nombres
@@ -11,58 +11,59 @@
  * Esta version nueva se hace ya que la idea de cambiar minusculas ha
  * generado varios problemas
  * Se incluye la logica de interpretacion de oraciones
+ * Se soluciona el problema de palabras compuestas
  */
 
-camino --> [[cartago], [san-jose], 25];
+concatenar([], L, L).
+concatenar([X|L1], L2, [X|L3]) :- concatenar(L1, L2, L3).
+
+camino --> [[cartago], [san, jose], 25];
            [[alajuela], [heredia], 7];
-           [[san-jose], [alajuela], 15];
-           [[cartago], [tres-rios], 10].
+           [[san, jose], [alajuela], 15];
+           [[cartago], [tres, rios], 10].
 
 origen(S,_) :- camino([S|_],_).
 destino(S,_) :- camino([_,S|_],_).
 
 lugar --> origen; destino.
 
+% El problema es que no se pueden concatenar listas como lo estoy haciendo
 % Analogo a: oracion(A, B) :- sintagma_nominal(A,C), sintagma_verbal(C,B).
-% Agregar caso: "Quiero ir a ..."
-% Caso 1:
-oracion --> sintagma_nominal(_Genero2, Numero), sintagma_verbal(Numero);
-            sintagma_nominal(_Genero2, Numero);
-            sintagma_verbal(Numero).
+oracion(PalabrasClave, Frase, []) :- sintagma_nominal(_, Numero, PalabrasClave1, Frase1, []),
+                                      sintagma_verbal(Numero, PalabrasClave2, Frase2, []),
+                                      concatenar(PalabrasClave1, PalabrasClave2, PalabrasClave),
+                                      concatenar(Frase1, Frase2, Frase);
+                                      sintagma_nominal(_, _, PalabrasClave, Frase, []);
+                                      sintagma_verbal(_, PalabrasClave, Frase, []).
+
+/*
+oracion([PalabrasClave1, PalabrasClave2]) --> sintagma_nominal(_, Numero, PalabrasClave1), sintagma_verbal(Numero, PalabrasClave2).
+oracion([PalabrasClave]) --> sintagma_nominal(_, _, PalabrasClave).
+oracion([PalabrasClave]) --> sintagma_verbal(_, PalabrasClave).
+*/
 
 % Caso 1: "el origen / es ..."
 % Caso 2: "mi origen / es ..."
 % Caso 3: "Cartago"
 % Caso 4: "yo / voy ..."
-sintagma_nominal(Genero, Numero) --> determinante(Genero, Numero), nombre(Genero, Numero);
-                                     determinante(impersonal, Numero), nombre(_Genero2, Numero);
-                                     nombre(Genero, Numero);
-                                     determinante(Genero, Numero).
+sintagma_nominal(Genero, Numero, [PalabrasClave]) --> determinante(Genero, Numero), nombre(Genero, Numero, PalabrasClave);
+                                                      determinante(impersonal, Numero), nombre(_Genero2, Numero, PalabrasClave);
+                                                      nombre(Genero, Numero, PalabrasClave).
+sintagma_nominal(Genero, Numero, [[]]) --> determinante(Genero, Numero).
 
 % Caso 1: "no"
 % Caso 2: "para Cartago"
-% Caso 3: "no voy para Cartago"
-% Caso 4: "voy PARA Cartago"
-sintagma_verbal(Numero) --> modulizador;
-                            enlace, sintagma_nominal(_Genero, _Numero2);
-                            modulizador, verbo_transitivo(Numero), enlace, sintagma_nominal(_Genero, _Numero2);
-                            verbo_transitivo(Numero), enlace, sintagma_nominal(_Genero, _Numero2).
-
-% Sinonimos para verbos transitivos
-sinonimo_verbo_transitivo(singular, ir) --> [quiero,ir].
-sinonimo_verbo_transitivo(singular, voy) --> [dirijo].
-sinonimo_verbo_transitivo(singular, estoy) --> [encuentro].
-
-% Sinonimos de nombres
-sinonimo_nombre(masculino, singular, origen) --> [punto,de,partida].
-sinonimo_nombre(masculino, singular, parada) --> [punto,intermedio]; [destino,intermedio].
-
-% Sinonimos de modulizadores
-sinonimo_modulizador(si) --> [afirmativo]; [claro]; [de,acuerdo]; [por,supuesto].
-sinonimo_modulizador(no) --> [negativo].
-
-% Sinonimos de enlaces
-sinonimo_enlace(para) --> [a].
+% Caso 3: "es Cartago"
+% Caso 4: "no voy para Cartago"
+% Caso 5: "voy PARA Cartago"
+sintagma_verbal(_, [PalabrasClave]) --> modulizador(PalabrasClave).
+sintagma_verbal(_, [PalabrasClave1, PalabrasClave2]) --> enlace(PalabrasClave1), sintagma_nominal(_, _, [PalabrasClave2]).
+sintagma_verbal(Numero, [PalabrasClave1, PalabrasClave2]) -->
+    verbo_transitivo(Numero, PalabrasClave1), sintagma_nominal(_, Numero, [PalabrasClave2]).
+sintagma_verbal(Numero, [PalabrasClave1, PalabrasClave2, PalabrasClave3]) -->
+    verbo_transitivo(Numero, PalabrasClave1), enlace(PalabrasClave2), sintagma_nominal(_, _, [PalabrasClave3]).
+sintagma_verbal(Numero, [PalabrasClave1, PalabrasClave2, PalabrasClave3, PalabrasClave4]) -->
+    modulizador(PalabrasClave1), verbo_transitivo(Numero, PalabrasClave2), enlace(PalabrasClave3), sintagma_nominal(_, _, [PalabrasClave4]).
 
 % Determinantes
 determinante(impersonal, singular) --> [mi].
@@ -70,27 +71,54 @@ determinante(masculino, singular) --> [el]; [un]; [yo]; [me].
 determinante(femenino, singular) --> [una]; [la].
 
 % Verbos
-verbo_transitivo(GeneroSinonimo) --> sinonimo_verbo_transitivo(GeneroSinonimo, _).
-verbo_transitivo(singular) --> [es]; [voy]; [estoy]; [ir]; [se,ubica]; [ubico].
+% verbo_transitivo(GeneroSinonimo) --> sinonimo_verbo_transitivo(GeneroSinonimo, _).
+% FALTAN SINONIMOS
+verbo_transitivo(singular, []) --> [es]; [se,ubica].
+verbo_transitivo(singular, [ubico]) --> [ubico].
+verbo_transitivo(singular, [voy]) --> [voy].
+verbo_transitivo(singular, [estoy]) --> [estoy].
+verbo_transitivo(singular, [ir]) --> [ir].
 
 % Nombres
 % Se manejaran casos con mayuscula desde Java.
-nombre(GeneroSinonimo, NumeroSinonimo) --> sinonimo_nombre(GeneroSinonimo, NumeroSinonimo, _).
-nombre(masculino, singular) --> [origen].
-nombre(masculino, singular) --> [destino].
-nombre(masculino, singular) --> lugar.
-nombre(femenino, singular) --> [parada].
+% FALTAN SINONIMOS
+nombre(masculino, singular, [origen]) --> [origen].
+nombre(masculino, singular, [destino]) --> [destino].
+nombre(masculino, singular, Lugar, Lugar, _) :- phrase(lugar, Lugar).
+nombre(femenino, singular, [parada]) --> [parada].
 
 % Modulizadores
-modulizador --> sinonimo_modulizador(_).
-modulizador --> [si]; [no].
+% modulizador --> sinonimo_modulizador(_).
+% FALTAN SINONIMOS
+modulizador([si]) --> [si].
+modulizador([no]) --> [no].
 
 % Enlaces
-enlace --> sinonimo_enlace(_).
-enlace --> [para]; [en].
+% enlace --> sinonimo_enlace(_).
+% FALTAN SINONIMOS
+enlace([para]) --> [para].
+enlace([en]) --> [en].
 
+/*
 % palabra --> lugar; determinante(_, _); verbo_transitivo(_); nombre(_,
 % _); modulizador; enlace.
+
+% Sinonimos para verbos transitivos
+sinonimo(verbo_transitivo, singular, ir) --> [quiero,ir].
+sinonimo(verbo_transitivo, singular, voy) --> [dirijo].
+sinonimo(verbo_transitivo, singular, estoy) --> [encuentro].
+
+% Sinonimos de nombres
+sinonimo(nombre, masculino, singular, origen) --> [punto,de,partida].
+sinonimo(nombre, masculino, singular, parada) --> [punto,intermedio]; [destino,intermedio].
+
+% Sinonimos de modulizadores
+sinonimo(modulizador, si) --> [afirmativo]; [claro]; [de,acuerdo]; [por,supuesto].
+sinonimo(modulizador, no) --> [negativo].
+
+% Sinonimos de enlaces
+sinonimo(enlace, para) --> [a].
+
 
 % Palabras clave, significado
 palabra_clave([origen], origen).
@@ -129,9 +157,6 @@ categorizar_palabras_clave([X|L1], L2, L3) :- (palabra_clave([X], T) -> categori
 
 gramatica_palabras_clave(L, G, []) :- categorizar_palabras_clave(L, L1), sort(L1, G).
 
-concatenar([], L, L).
-concatenar([X|L1], L2, [X|L3]) :- concatenar(L1, L2, L3).
-
 autollenar(origen, Oracion, Resultado) :- concatenar([estoy, en], Oracion, Resultado).
 
 main :- writeln("Origen?"),
@@ -149,7 +174,7 @@ validar(Objetivo) :- repeat,
                                 true;
                                 write("Comprendo lo que dices pero no puedo responderte con esa informacion. "), preguntar(Objetivo), fail);
                            writeln("No te entiendo. "), preguntar(Objetivo), fail).
-
+*/
 
 /*
  * - Origen: Hola WazeLog estoy en Cartago
