24051ea042f91d92880d0188650a1413b5571539
Eduardo Moya <e.d.mobe@hotmail.com>
2019-05-28 15:54:33 -0600

    4

--- lexical_test.pl
+++ lexical_test.pl
@@ -1,45 +1,80 @@
 /*
- * Ejemplo 6.2.3.
+ * Ejemplo 6.2.4.
  * Se busca simplificar la notacion de las gramaticas libres de contexto
  * Segun lo investigado, hay otra manera de expresar las gramaticas
+ * Se implementa logica que permite utilizar los caminos como nombres
+ * Compatible con sinonimos
+ * Se investiga como manejar diferencias de palabras por capitalizacion
+ * Cambios de optimizacion de codigo
  * Fuente: https://www.cs.us.es/cursos/ia2-2004/temas/tema-03.pdf
  * Fuente: https://personal.us.es/fsoler/papers/05capgram.pdf
 */
 
-camino('Cartago',"San Jose",15).
+camino('Cartago', "San Jose", 15).
 
-% Se agrega el caso del sujeto omitido para casos como "Quiero ir a ..."
-% Se agrega el caso del predicado omitido para casos como "Cartago"
+% Auxiliar para obtener una oracion en minuscula
+% string_lower("WazeLog",X)
+minuscula([], _).
+minuscula([X|L1], [Y|L2]):- string_lower(X,Y), minuscula(L1, L2).
+
+% Analogo a: oracion(A, B) :- sintagma_nominal(A,C), sintagma_verbal(C,B).
+% Agregar caso: "Quiero ir a ..."
+% Caso 1:
 oracion --> sintagma_nominal(_Genero2, Numero), sintagma_verbal(Numero);
-sintagma_nominal(_Genero2,Numero);
-sintagma_verbal(Numero).
+            sintagma_nominal(_Genero2, Numero);
+            sintagma_verbal(Numero).
 
+% Caso 1: "el origen / es ..."
+% Caso 2: "mi origen / es ..."
+% Caso 3: "Cartago"
+% Caso 4: "yo / voy ..."
+sintagma_nominal(Genero, Numero) --> determinante(Genero, Numero), nombre(Genero, Numero);
+                                     determinante(impersonal, Numero), nombre(_Genero2, Numero);
+                                     nombre(Genero, Numero);
+                                     determinante(Genero, Numero).
 
-% Se agrega el caso de impersonal para casos como "mi origen es ..."
-% Se agrega el caso de determinante omitido para casos como "Cartago"
-sintagma_nominal(Genero,Numero) --> determinante(Genero,Numero), nombre(Genero,Numero);
-determinante(impersonal,Numero), nombre(_Genero2,Numero);
-nombre(Genero,Numero).
-
-% Se agrega el caso de solamente el modulizador para casos como "no"
-% Se agregan un caso de enlace para casos como "voy PARA Cartago"
-% Se agregan un caso de enlace para casos como "PARA Cartago"
+% Caso 1: "no"
+% Caso 2: "para Cartago"
+% Caso 3: "no voy para Cartago"
+% Caso 4: "voy PARA Cartago"
 sintagma_verbal(Numero) --> modulizador;
-modulizador, verbo_transitivo(Numero), sintagma_nominal(_Genero,_Numero2);
-verbo_transitivo(Numero),sintagma_nominal(_Genero,_Numero2);
-modulizador, verbo_transitivo(Numero), enlace, sintagma_nominal(_Genero,_Numero2);
-verbo_transitivo(Numero),sintagma_nominal(_Genero,_Numero2).
+                            verbo_transitivo(Numero), sintagma_nominal(_Genero, _Numero2);
+                            modulizador, verbo_transitivo(Numero), enlace, sintagma_nominal(_Genero, _Numero2);
+                            verbo_transitivo(Numero), enlace, sintagma_nominal(_Genero, _Numero2).
 
-determinante(impersonal,singular) --> [mi].
-determinante(masculino,singular) --> [el];[un].
+% Sinonimos de nombres
+sinonimo(origen, "punto de partida").
 
-verbo_transitivo(singular) --> [es].
+% Sinonimos de modulizadores
+sinonimo(si, sí).
+sinonimo(si, afirmativo).
+sinonimo(si, claro).
+sinonimo(si, "de acuerdo").
+sinonimo(si, "por supuesto").
+sinonimo(no, negativo).
 
-nombre(masculino,singular) --> [origen];[destino];["punto de partida"];[parada];['WazeLog'];['Wazelog'];[wazelog];['Cartago'].
+% Sinonimos de enlaces
+sinonimo(para, a).
 
-modulizador --> [sí];[si];[no].
+% Determinantes
+determinante(impersonal, singular) --> [mi].
+determinante(masculino, singular) --> [el]; [un]; [yo].
 
-enlace --> [para];[en].
+% Verbos
+verbo_transitivo(singular) --> [es]; [voy].
+
+% Nombres
+% Se manejaran casos como "WazeLog" o "Wazelog" con string_lower("WazeLog",X).
+nombre(masculino, singular) --> [origen]; [destino]; [parada]; [wazelog].
+nombre(masculino, singular, [S|_], _) :- sinonimo(origen, S).
+nombre(masculino, singular, [X|_], _) :- camino(X, _Lugar2, _Peso).
+nombre(masculino, singular, [Y|_], _) :- camino(_Lugar1, Y, _Peso).
+
+modulizador --> [si]; [no].
+modulizador([S|_], _) :- sinonimo(si, S).
+
+enlace --> [para]; [en].
+enlace([S|_], _) :- sinonimo(para, S).
 
 /*
  * - Origen: Hola WazeLog estoy en Cartago
@@ -55,3 +90,9 @@ enlace --> [para];[en].
 "Voy para X", "Mi destino es X", "Me dirijo a X"
 "Estoy en Y", "Me encuentro en Y", "Parto de Y", "Mi origen es Y", "El origen del viaje es Y"
 */
+
+
+
+
+
+
